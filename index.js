9452e17d64fe9118c007894a2162bd4192f119f1
Rishi Panthee <rrpanthe@ncsu.edu>
2021-01-19 12:48:50 -0600

    update

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let hive = require("@hiveio/hive-js")
 let logger = require("node-color-log")
 
 let rpcAPI = "https://api.hive-engine.com/rpc/contracts"
-let markets = ["STARBITS", "SIM", "BEER", "HUSTLER", "LEO", "LIST"]
-let username = "rishi556"
+let markets = ["STARBITS", "SIM", "BEER", "HUSTLER", "LEO", "LIST", "ECU", "WEED"]
+let username = "deathbing"
 let privateActiveKey = ""
 
 async function cancelAllOrders() {
@@ -19,13 +19,11 @@ async function cancelAllOrders() {
     cancelJSON.push({ "contractName": "market", "contractAction": "cancel", "contractPayload": { "type": "sell", "id": `${orders[i].txId}` } })
   }
   hive.broadcast.customJson(privateActiveKey, [username], null, "ssc-mainnet-hive", JSON.stringify(cancelJSON), (err) => {
-    if (err){
+    if (err) {
       logger.error(`Error cancelling, recieved error : ${err}, JSON attempted to broadcst: ${JSON.stringify(cancelJSON)}.`)
       throw new Error(err)
     }
-    setTimeout(() => {
-      return cancelAllOrders()
-    }, 1000 * 10)
+    return true
   })
 }
 
@@ -34,9 +32,9 @@ async function getBalances() {
   let result = await axios.post(rpcAPI, getBalancesQuery)
   let balances = result.data.result
   let nonZeroBalances = {}
-  for (let i in balances){
-    if (parseFloat(balances[i].balance) > 0){
-      nonZeroBalances[balances[i].symbol] = balances[i].balance
+  for (let i in balances) {
+    if (parseFloat(balances[i].balance) > 0) {
+      nonZeroBalances[balances[i].symbol] = parseFloat(balances[i].balance) / 7.69
     }
   }
   return nonZeroBalances
@@ -47,40 +45,42 @@ async function getPrices(tokens) {
   let result = await axios.post(rpcAPI, metricsQuery)
   let prices = result.data.result
   let validPrices = {}
-  for (let i in prices){
-    if (parseFloat(prices[i].lowestAsk) !== 0){
+  for (let i in prices) {
+    if (parseFloat(prices[i].lowestAsk) !== 0) {
       validPrices[prices[i].symbol] = prices[i].lowestAsk
     }
   }
   return validPrices
 }
 
-function placeOrders(balances, prices){
+function placeOrders(balances, prices) {
   let orderJSON = []
-  for (let i in balances){
-    if (prices[i]){
-      orderJSON.push({ "contractName": "market", "contractAction": `sell`, "contractPayload": { "symbol": i, "quantity": `${balances[i]}`, "price": `${(parseFloat(prices[i]) - 0.00000001).toFixed(8)}` } })
+  for (let i in balances) {
+    if (prices[i]) {
+      orderJSON.push({ "contractName": "market", "contractAction": `sell`, "contractPayload": { "symbol": i, "quantity": `${parseInt(balances[i])}`, "price": `${(parseFloat(prices[i]) - 0.00000001).toFixed(8)}` } })
     }
   }
-  if (orderJSON.length === 0){
+  if (orderJSON.length === 0) {
     return
   }
   hive.broadcast.customJson(privateActiveKey, [username], null, "ssc-mainnet-hive", JSON.stringify(orderJSON), (err) => {
-    if (err){
+    if (err) {
       logger.error(`Error placing order, recieved error : ${err}, JSON attempted to broadcst: ${JSON.stringify(orderJSON)}.`)
       throw new Error(err)
     }
   })
 }
 
-async function refresh(){
+async function refresh() {
   await cancelAllOrders()
-  let balances = await getBalances()
-  let prices = await getPrices(Object.keys(balances))
-  placeOrders(balances, prices)
+  setTimeout(async () => {
+    let balances = await getBalances()
+    let prices = await getPrices(Object.keys(balances))
+    placeOrders(balances, prices)
+  }, 1000 * 3)
 }
 
 refresh()
 setInterval(() => {
   refresh()
-}, 10 * 1000 * 5)
+}, 1000 * 60 * 3.5)
